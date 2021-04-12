22584dc6fb732a907a2b7c61b7b8355449dc06da
kartexxx <81906852+kartexxx@users.noreply.github.com>
2021-04-12 13:42:52 +0300

    Create sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,5 +1,5 @@
-API_ID = 14371
-API_HASH = "e46b6c854d2bf58a0"
+API_ID = 1777719241:AAEv9r6QgNHU5zhyxBNTiD1J5diD_ohj4TQ
+API_HASH = "5514c685f88de7200f81165998c91ee1"
 SUDO_CHAT_ID = -1001485876964  # Chat where the bot will play the music.
 SUDOERS = [1243703097, 13216546] # Users which have special control over the bot.
 
